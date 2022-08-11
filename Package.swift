abfd956bbedd17369353077baf5eeeb39ce0d626
ChangGwiLee <lcg5450@gmail.com>
2022-08-11 16:29:03 +0900

    안되서 모두 커밋

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Cachy",
-    platforms: [.iOS(.v9)],
+    platforms: [
+      .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v5)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
